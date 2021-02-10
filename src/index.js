bcd3e2ef0e9b2f85664f90bd22fb91676c7b4f29
Ruslan Chirski <ruslan.chirski@gmail.com>
2021-02-10 11:14:22 +0300

    bug fixes

--- src/index.js
+++ src/index.js
@@ -6,5 +6,11 @@ import App from './components/App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 
+const getData = async () => {
+  const response = await fetch('https://swapi.dev/api/films/1/')
+  console.log(response);
+}
+
+getData();
 
 ReactDom.render(<App />, document.getElementById('root'));
